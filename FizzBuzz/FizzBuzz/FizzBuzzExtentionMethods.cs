5cac6ab0e87ed4f3c8607b4b4823e50750c4959d
Khosro Jadidi <khosro.jadidi@consultant.pulsen.se>
2020-11-23 17:43:43 +0100

    added extention methods

---
+++ FizzBuzz/FizzBuzz/FizzBuzzExtentionMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public static class FizzBuzzExtentionMethods
+    {
+        public static  string CheckForFizz(this int value)
+        {
+            if (value % 3 == 0)
+                return "Fizz";
+            return String.Empty;
+        }
+
+        public static string CheckForBuzz(this int value)
+        {
+            if (value % 5 == 0)
+                return "Buzz";
+            return String.Empty;
+        }
+
+        public static string GetAnswerOfLife(this int value)
+        {
+            if (value == 42)
+                return "Answer to the Ultimate Question of Life, the Universe, and Everything";
+            return String.Empty;
+        }
+    }
+}
--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -1,34 +1,33 @@
 ﻿using System;
 
+
 namespace FizzBuzz
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             var program = new Program();
             program.PrintFizzBuzz();
-
-            //PrintFizzBuzz();
         }
 
         public void PrintFizzBuzz()
         {
             for (int i = 1; i < 300; i++)
             {
+                string text;
 
-                string text = string.Empty;
-                if (i % 3 == 0)
-                    text += "Fizz";
-                if (i % 5 == 0)
-                    text += "Buzz";
-                if (i == 42)
-                    text = "Answer to the Ultimate Question of Life, the Universe, and Everything";
+                text = i.GetAnswerOfLife();
+                if (text.Length == 0)
+                {
+                    text += i.CheckForFizz();
+                    text += i.CheckForBuzz();
+                }
                 if (text.Length == 0)
                     text = i.ToString();
 
                 Console.WriteLine(text);
             }
-        }
+        }        
     }
 }
